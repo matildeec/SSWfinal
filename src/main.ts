27169157ffc569e90b6fc155d7044d77799b5159
matildeec <125213336+matildeec@users.noreply.github.com>
2023-06-26 15:02:02 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
 import 'zone.js/dist/zone';
-import 'zone.js/dist/zone';
-import { Component, importProvidersFrom } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {RootComponent} from "./root/root.component";
-import {RouterModule} from "@angular/router";
-import { routes } from './root/routes';
-//import {routes} from "./app/routes";
+import { RootComponent } from "./root/root.component";
 
-bootstrapApplication(RootComponent, {
-  providers: [
-    importProvidersFrom(RouterModule.forRoot(routes))]
-})
-.catch(err => console.error(err));
+bootstrapApplication(RootComponent).catch(err => console.error(err));
